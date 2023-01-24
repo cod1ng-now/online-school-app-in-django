ad2e3cdd9c089516e6a4f7b51ca0bca2ed8a94d0
cod1ng-now <muhammmad.axmedov.2000@bk.ru>
2023-01-24 22:19:11 +0500

    cod1ng_now

---
+++ main/models(1).py
@@ -0,0 +1,33 @@
+from distutils.command.upload import upload
+from django.db import models
+
+# Create your models here.
+
+
+class Playlist(models.Model):
+    nomi = models.CharField(max_length=150)
+    image = models.ImageField(upload_to = 'rasmlar/playlist_rasm')
+    sana = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nomi
+    
+    
+    
+class VideoDars(models.Model):
+    nomi = models.CharField(max_length=150)
+    rasm = models.ImageField(upload_to='video_darslar/rasmlar')
+    izoh = models.TextField()
+    video = models.FileField(upload_to='video_darslar/video')
+    playlisti = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    # sana = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.nomi
+    
+    
+# class Teachers(models.Model):
+#     ismi = models.CharField(max_length=150)
+#     rasmi = models.ImageField(upload_to = 'teachers/rasmi')
+    
--- main/models.py
+++ main/models.py
@@ -1,36 +1,32 @@
+from distutils.command.upload import upload
 from django.db import models
 
-class Category(models.Model):
-    title=models.CharField(max_length=200)
-    category_image=models.ImageField(upload_to='imgs/')
+# Create your models here.
 
-    class Meta:
-        verbose_name_plural='Categories'
 
+class Playlist(models.Model):
+    nomi = models.CharField(max_length=150)
+    image = models.ImageField(upload_to = 'rasmlar/playlist_rasm')
+    sana = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
-        return self.title
+        return self.nomi
 
-# News Model
-class News(models.Model):
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    title=models.CharField(max_length=300)
-    image=models.ImageField(upload_to='imgs/')
-    detail=models.TextField()
-    add_time=models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural='News'
-
+    
+class VideoDars(models.Model):
+    nomi = models.CharField(max_length=150)
+    rasm = models.ImageField(upload_to='video_darslar/rasmlar')
+    izoh = models.TextField()
+    video = models.FileField(upload_to='video_darslar/video')
+    playlisti = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    # sana = models.DateTimeField(auto_now_add=True)
+    
+    
     def __str__(self):
-        return self.title
-
-# Comments
-class Comment(models.Model):
-    news=models.ForeignKey(News,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100)
-    email=models.CharField(max_length=200)
-    comment=models.TextField()
-    status=models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.comment
+        return self.nomi
+    
+    
+# class Teachers(models.Model):
+#     ismi = models.CharField(max_length=150)
+#     rasmi = models.ImageField(upload_to = 'teachers/rasmi')
+    
